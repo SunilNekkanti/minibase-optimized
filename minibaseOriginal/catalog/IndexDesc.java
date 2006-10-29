a3ef2078ee13e8e66fbf5b14a1f9a2f3d20abd5e
fran6co <fran6co@c0cf2cfa-af1d-0410-91bb-fff344c7b4b4>
2006-10-29 22:12:53 +0000

    Arregle un par de bugs mas, y estoy completando de pasar uno de los test

--- minibaseOriginal/catalog/IndexDesc.java
+++ minibaseOriginal/catalog/IndexDesc.java
@@ -1,27 +1,37 @@
 //------------------------------------
-// IndexDesc.java
-//
-// Ning Wang, April,24  1998
+//IndexDesc.java
+
+//Ning Wang, April,24  1998
 //-------------------------------------
 
 package catalog;
 
-import bufmgr.*;
-import diskmgr.*;
-import global.*;
+import global.IndexType;
+import global.TupleOrder;
 
-// IndexDesc class: schema for index catalog
+//IndexDesc class: schema for index catalog
 /**
  * @author  Fernando
  */
 public class IndexDesc
 {
-    	String relName;                     // relation name
+	String indexName;
+	String relName;                     // relation name
 	String attrName;                    // attribute name
 	IndexType  accessType;                // access method
 	TupleOrder order;                     // order of keys
 	int        clustered = 0;                 //
 	int        distinctKeys = 0;              // no of distinct key values
 	int        indexPages = 0;                // no of index pages
+
+	public String getIndexName() {
+		return indexName;
+	}
+	public IndexType getIndexType(){
+		return accessType;
+	}
+	public String getRelationName(){
+		return relName;
+	}
 };
 
